a30d7089bcc1967749bbc151ff3d841e92cd347c
valeriusargus <valerius@embauchez-moi.com>
2019-08-07 11:52:28 +0200

    Valere est passé par là...

--- machineASous/src/app/p2/p2.page.ts
+++ machineASous/src/app/p2/p2.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from "jquery";
-import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-p2',
@@ -10,18 +9,21 @@ import { NavController } from '@ionic/angular';
 export class P2Page implements OnInit {
 
     trials = 5;
+    bank = 0;
+    bankStr = '00000';
 
-    constructor(public nav : NavController) { }
+  constructor() { }
 
-  goHome() {
-    this.nav.navigateForward("home");
-  }
   ngOnInit() {
     const context = this;
-            const path = '../../assets/photo/';
-            var genTime = null, tr1 = null, tr2 = null, tr3 = null;
+    const photopath = '../../assets/photo/';
+    const ambSnd = $('audio#ambiencesnd')[0];
+    let genTime = null, tr1 = null, tr2 = null, tr3 = null;
     let rolling = false, r1 = false, r2 = false, r3 = false;
-    var rndHead, r1Rnd, r2Rnd, r3Rnd;
+    let rndHead, r1Rnd, r2Rnd, r3Rnd;
+
+    ambSnd.volume = 1;
+    ambSnd.play();
 
     $('#activate').click(function() {
         if (rolling == false) {
@@ -49,40 +51,46 @@ export class P2Page implements OnInit {
 
     function playing() {
 
-            if (r1 == true) {
-                r1Rnd = Math.floor(Math.random() * 6) + 1;
-                rndHead = 'url(' + path + String(r1Rnd) + '.png)';
-                $('#roll1').css('background-image', rndHead);
-            }
-
-            if (r2 == true) {
-                r2Rnd = Math.floor(Math.random() * 6) + 1;
-                rndHead = 'url(' + path + String(r2Rnd) + '.png)';
-                $('#roll2').css('background-image', rndHead);
-            }
-            
-            if (r3 == true) {
-                r3Rnd = Math.floor(Math.random() * 6) + 1;
-                rndHead = 'url(' + path + String(r3Rnd) + '.png)';
-                $('#roll3').css('background-image', rndHead);
-            }
+        if (r1 == true) {
+            r1Rnd = Math.floor(Math.random() * 6) + 1;
+            rndHead = 'url(' + photopath + String(r1Rnd) + '.png)';
+            $('#roll1').css('background-image', rndHead);
         }
 
-        function verdict () {
-            clearInterval(genTime);
-            clearTimeout(tr1);
-            clearTimeout(tr2);
-            clearTimeout(tr3);
-            if (r1Rnd == r2Rnd == r3Rnd) {
-                alert("Vous avez gagné en " + (5 - context.trials) + " essai(s)!");
-                context.trials = 5;
-            }
-            else if (context.trials > 0) {
-                alert("Perdu! Il vous reste encore " + context.trials + " essai(s)...");
-            }
-            else {
-                alert("Perdu! Vous avez utilisé vos cinq essais, vous ne pouvez plus jouer!");
-            }
+        if (r2 == true) {
+            r2Rnd = Math.floor(Math.random() * 6) + 1;
+            rndHead = 'url(' + photopath + String(r2Rnd) + '.png)';
+            $('#roll2').css('background-image', rndHead);
+        }
+            
+        if (r3 == true) {
+            r3Rnd = Math.floor(Math.random() * 6) + 1;
+            rndHead = 'url(' + photopath + String(r3Rnd) + '.png)';
+            $('#roll3').css('background-image', rndHead);
+        }
+    }
+
+    function verdict () {
+
+        clearInterval(genTime);
+        clearTimeout(tr1);
+        clearTimeout(tr2);
+        clearTimeout(tr3);
+
+        r1Rnd = 1; r2Rnd = 1; r3Rnd = 1;
+
+        if ((r1Rnd == r2Rnd) && (r1Rnd == r3Rnd)) {
+            alert("Vous avez gagné en " + (5 - context.trials) + " essai(s)!");
+            context.bank += 250;
+            context.bankStr = context.bank.toString().padStart(5, '0');
+            context.trials = 5;
+        }
+        else if (context.trials > 0) {
+            alert("Perdu! Il vous reste encore " + context.trials + " essai(s)...");
+        }
+        else {
+            alert("Perdu! Vous avez utilisé vos cinq essais, vous ne pouvez plus jouer!");
+        }
             rolling = false;
         }
     }
